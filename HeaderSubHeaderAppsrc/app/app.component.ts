32f1c192fc1cd3170769c4ec2dae4a56a8a6452a
bharatibelhekar <40517732+bharatibelhekar@users.noreply.github.com>
2018-07-02 16:05:09 +0530

    Assign1-4

---
+++ HeaderSubHeaderAppsrc/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  header = 'Marvellous Infosystems';
+  subHeader = 'Educating for better tomorrow';
+}
